5aa20b5c126141482c859818742f7bfc262b7b9d
rc-balaji <rcbalaji2003@gmail.com>
2024-06-24 14:16:02 +0530

    Cors

--- server.js
+++ server.js
@@ -38,6 +38,40 @@ app.post('/webhook/get_city', (req, res) => {
     }
 });
 
+app.post('/webhook', (req, res) => {
+    const agent = new WebhookClient({ request: req, response: res });
+  
+    function handleIntent(agent) {
+      const response = {
+        "richContent": [
+          [
+            {
+              "type": "button",
+              "icon": {
+                "type": "chevron_right",
+                "color": "#FF9800"
+              },
+              "text": "Click here to visit Google.com",
+              "link": "https://www.google.com"
+            }
+          ]
+        ]
+      }
+      
+  
+      agent.add(new Payload(agent.UNSPECIFIED, response, { rawPayload: true }));
+  
+      // If you need to handle additional logic based on user input, you can add it here
+    }
+  
+    let intentMap = new Map();
+    intentMap.set('URL', handleIntent); // Replace 'yourIntentName' with your actual intent name
+    agent.handleRequest(intentMap);
+  });
+  
+  
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
